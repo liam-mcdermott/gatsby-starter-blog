73ffb1bfbfe6c456b5bb94051634a14a85201d12
Liam McDermott <liam.mcdermott@madetoengage.com>
2019-07-29 23:02:27 +0100

    comment out other stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,15 +15,15 @@ module.exports = {
       {
         trackingId: "UA-53312935-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
+        head: true
         // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
+        // anonymize: true,
+        // // Setting this parameter is also optional
+        // respectDNT: true,
+        // // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // // Delays sending pageview hits on route update (in milliseconds)
+        // pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
         //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // Enables Google Optimize Experiment ID
@@ -31,9 +31,9 @@ module.exports = {
         // Set Variation ID. 0 for original 1,2,3....
         //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
         // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "liammcdermott.me.uk",
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "liammcdermott.me.uk",
       }
     },
     {
